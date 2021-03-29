178a9142290d37aade9cfcd9d3a3fa7c0bb34102
Anish-Coding <73768987+Anish-Coding@users.noreply.github.com>
2021-03-29 17:10:38 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -9,7 +9,7 @@ class BaseClass{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("sprites/wood1.png");
+        this.image = loadImage("download.png");
         World.add(world, this.body);
       }
       display(){
